18420256ea55915d5d5aa13f00ad1d744092dab8
ixel05 <ixelsandoval05@gmail.com>
2023-04-01 20:35:43 -0600

    create CRUD api rest marcas

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { CategoriasModule } from './categorias/categorias.module';
 import { CiudadesModule } from './ciudades/ciudades.module';
 import { GenerosModule } from './generos/generos.module';
+import { MarcasModule } from './marcas/marcas.module';
 
 @Module({
-  imports: [CategoriasModule, CiudadesModule, GenerosModule],
+  imports: [CategoriasModule, CiudadesModule, GenerosModule, MarcasModule],
   controllers: [AppController],
   providers: [AppService],
 })
