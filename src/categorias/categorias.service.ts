1815a8c2169ef4f762b62e2b0de3576be70cbbc6
ixel05 <ixelsandoval05@gmail.com>
2023-04-01 22:10:35 -0600

    terminado crud  con categorias

--- src/categorias/categorias.service.ts
+++ src/categorias/categorias.service.ts
@@ -1,26 +1,46 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCategoriaDto } from './dto/create-categoria.dto';
 import { UpdateCategoriaDto } from './dto/update-categoria.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Categoria } from './entities/categoria.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CategoriasService {
-  create(createCategoriaDto: CreateCategoriaDto) {
-    return 'This action adds a new categoria';
+  constructor(
+    @InjectRepository(Categoria)
+    private categoriaRepository: Repository<Categoria>,
+  ) {}
+
+  async create(categoriaDto: CreateCategoriaDto) {
+    const categoria = this.categoriaRepository.create(categoriaDto);
+    await this.categoriaRepository.save(categoria);
+
+    return categoria;
   }
 
   findAll() {
-    return `This action returns all categorias`;
+    return this.categoriaRepository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} categoria`;
+    return this.categoriaRepository.findOneBy({ id });
   }
 
-  update(id: number, updateCategoriaDto: UpdateCategoriaDto) {
-    return `This action updates a #${id} categoria`;
+  async  update(id: number, updatecategoriaDto:  UpdateCategoriaDto) {
+    const findCategoria = await this.findOne(id);
+    const updatecategoria = await this.categoriaRepository.merge(
+      findCategoria,
+      updatecategoriaDto
+    );
+
+    return this.categoriaRepository.save(updatecategoria);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} categoria`;
+  
+  async remove(id: number) {
+    const categoria = await this.findOne(id);
+    await this.categoriaRepository.remove(categoria);
+    return `categoria eliminada`;
   }
 }
