586a42706d7fe5cbc83bec53c8ab3cab45fe4652
ixel05 <ixelsandoval05@gmail.com>
2023-04-01 22:02:17 -0600

    CRUD API ciudad terminado

--- src/ciudades/ciudades.module.ts
+++ src/ciudades/ciudades.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CiudadesService } from './ciudades.service';
 import { CiudadesController } from './ciudades.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Ciudade } from './entities/ciudade.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Ciudade])],
   controllers: [CiudadesController],
   providers: [CiudadesService]
 })
--- src/ciudades/ciudades.service.ts
+++ src/ciudades/ciudades.service.ts
@@ -1,26 +1,46 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCiudadeDto } from './dto/create-ciudade.dto';
 import { UpdateCiudadeDto } from './dto/update-ciudade.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Ciudade } from './entities/ciudade.entity';
 
 @Injectable()
 export class CiudadesService {
-  create(createCiudadeDto: CreateCiudadeDto) {
-    return 'This action adds a new ciudade';
+  constructor(
+    @InjectRepository(Ciudade)
+    private ciudadRepository: Repository<Ciudade>,
+  ) {}
+
+  
+  async create(ciudadDto: CreateCiudadeDto) {
+    const ciudad = this.ciudadRepository.create(ciudadDto);
+    await this.ciudadRepository.save(ciudad);
+
+    return ciudad;
   }
 
   findAll() {
-    return `This action returns all ciudades`;
+    return this.ciudadRepository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} ciudade`;
+    return this.ciudadRepository.findOneBy({ id });
   }
 
-  update(id: number, updateCiudadeDto: UpdateCiudadeDto) {
-    return `This action updates a #${id} ciudade`;
+  async  update(id: number, updateCiudadDto: UpdateCiudadeDto) {
+    const findciudad = await this.findOne(id);
+    const updateProduct = await this.ciudadRepository.merge(
+      findciudad,
+      updateCiudadDto
+    );
+
+    return this.ciudadRepository.save(updateProduct);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} ciudade`;
+  async remove(id: number) {
+    const ciudad = await this.findOne(id);
+    await this.ciudadRepository.remove(ciudad);
+    return `Ciudad eliminada`;
   }
 }
